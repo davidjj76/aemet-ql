74ac353981a1af38c22b8c5f7d058607311bfce1
David Jiménez <davidjj76@gmail.com>
2018-06-20 23:34:12 +0200

    Changes in linter and prettier

--- server.js
+++ server.js
@@ -3,12 +3,26 @@ const graphqlHTTP = require('express-graphql');
 const { buildSchema } = require('graphql');
 
 const schema = buildSchema(`
+  type User {
+    id: String
+    name: String
+  }
+
   type Query {
-    hello: String
+    user(id: !String): User
   }
 `);
+class User {
+  constructor(id) {
+    this.id = id;
+  }
 
-const rootValue = { hello: () => 'Hello world!' };
+  name() {
+    return `${this.id}-'David'`;
+  }
+}
+
+const rootValue = { user: ({ id }) => new User(id) };
 
 const app = express();
 app.use('/graphql', graphqlHTTP({ graphiql: true, rootValue, schema }));
